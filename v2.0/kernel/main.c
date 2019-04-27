1f88d937b26a544bd7c22281d8dea51341ce5280
marin9 <marin.bralic09@gmail.com>
2019-04-27 19:29:46 +0200

    Update

--- v2.0/kernel/main.c
+++ v2.0/kernel/main.c
@@ -20,9 +20,6 @@ void startup(){
 }
 
 /*
-
-
-
 #define KMI_KB_BASE	            0x10006000	// keyboard
 #define KMI_MS_BASE	            0x19000000	// mouse
 
@@ -38,17 +35,15 @@ volatile KMI_MMIO *mmio;
 unsigned int tmp;
 
 
-
-
 void startup(){
 	stdio_init();
 
-	*(volatile unsigned int *)(0x1000001C) = 0x2CAC; /* timing magic for SVGA 800x600 */
+	*(volatile unsigned int *)(0x1000001C) = 0x2CAC;  timing magic for SVGA 800x600 
 	*(volatile unsigned int *)(0x10120000) = 0x1313A4C4;
 	*(volatile unsigned int *)(0x10120004) = 0x0505F657;
 	*(volatile unsigned int *)(0x10120008) = 0x071F1800;
-	*(volatile unsigned int *)(0x10120010) = (1 * 1024 * 1024); /* base addr of frame buffer */
-	*(volatile unsigned int *)(0x10120018) = 0x82b; /* control bits */
+	*(volatile unsigned int *)(0x10120010) = (1 * 1024 * 1024);  base addr of frame buffer 
+	*(volatile unsigned int *)(0x10120018) = 0x82b;  control bits 
 	int i;
 	volatile unsigned int *p=*(volatile unsigned int *)(0x10120010);
 	for(i=0;i<1*1024*1024;++i){
@@ -58,13 +53,13 @@ void startup(){
 
 	
 	mmio=(KMI_MMIO*)KMI_KB_BASE;
-mmio->cr = 0x14;
-mmio->clk = 15;
+	mmio->cr = 0x14;
+	mmio->clk = 15;
 
 	while(1){
 		for(i=0;i<50000000;++i) asm volatile("");
 		printf("data: %x\nstat: %x\n\n", mmio->data, mmio->stat);
 	}
-	while(1);	
-
+	while(1);
 */
+

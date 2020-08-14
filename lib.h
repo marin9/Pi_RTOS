0f9f8bd6fc464d55abeea586e5bd85a15ffdde81
marin9 <marin.bralic09@gmail.com>
2020-08-14 13:21:37 +0200

    update lib

--- lib.h
+++ lib.h
@@ -1,12 +1,25 @@
 #pragma once
 
-#define BIT_VAL(n) 			(1<<n)
-#define BIT_GET(x, n) 		((x>>n)&1)
-#define BIT_SET(x, n)		(x|(1<<n))
-#define BIT_CLR(x, n)		(x&(~(1<<n)))
-#define BIT_INV(x, n)		(x^(1<<n))
-#define BIT_WR(x, n, v)		((x&(~(1<<n)))|((v&1)<<n))
+#define BIT_VAL(n) 			(1 << (n))
+#define BIT_GET(x, n) 		(((x) >> (n)) & 1)
+#define BIT_SET(x, n)		((x) | (1 << (n)))
+#define BIT_CLR(x, n)		((x) & ( ~(1 << (n))))
+#define BIT_INV(x, n)		((x) ^ (1 << (n)))
+#define BIT_WR(x, n, v)		(((x) & ( ~(1 << (n)))) | (((v) & 1) << (n)))
 
+#define DEC2ASCII(dec)		((dec) + 0x30)
+#define ASCII2DEC(asc)		((asc) - 0x30)
+#define HEX2ASCII(hex)  	(((hex) > 0x09) ? ((hex) + 0x37): ((hex) + 0x30)) 
+#define ASCII2HEX(asc)  	(((asc) > 0x39) ? ((asc) - 0x37): ((asc) - 0x30))
+
+#define GET_BYTE0(b)		((b >> 0) & 0xFF)
+#define GET_BYTE1(b)		((b >> 8) & 0xFF)
+#define GET_BYTE2(b)		((b >> 16) & 0xFF)
+#define GET_BYTE3(b)		((b >> 24) & 0xFF)
+
+#define GET_MAX(a, b)		(((a) > (b))? (a): (b))
+#define GET_MIN(a, b)		(((a) < (b))? (a): (b))
+#define GET_ABS(a)			(((a) < 0)? -(a): (a))
 
 void *memset(void *s, int c, int n);
 void *memcpy(void *dest, void *src, int n);
